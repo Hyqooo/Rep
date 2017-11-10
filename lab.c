1c79f95235a0d4448f0e50475b255c46b6a8738e
Hyqooo <33322066+Hyqooo@users.noreply.github.com>
2017-11-10 23:50:58 +0300

    Update lab.c

--- lab.c
+++ lab.c
@@ -19,18 +19,18 @@ typedef struct List{
 int CheckInput(char);
 void IsAllocated(void *);
 void AddToBegin(List_t **, Word_t *);
-struct List * AddToEnd(List_t *, Word_t *);
 void AddBet(List_t *, List_t *, Word_t *);
-void AddFromQ(List_t *, List_t *, List_t *);
+void DeleteSpec(List_t  **, List_t *);
+struct List * AddToEnd(List_t *, Word_t *);
 
 void main(){
-	Word_t *aWords = NULL;
-	List_t *listHead = NULL, *queueHead = NULL, *currentList = NULL;
+	Word_t *aWords = NULL, *curWord = NULL;
+	List_t **listHead = NULL, *queueHead = NULL, *currentList = NULL, *curQueue = NULL;
 	
 	char *sent = NULL, **words = NULL;
-	int i, j = 0, wordCounter = 0, oldLen = 0, fAdded = 0;
+	int i, j = 0, k = 0, wordCounter = 0, oldLen = 0, fAdded = 0, listsA = 0;
 
-	// Записываем слова в предложение
+	// Записываем слова в предложение.
 	for (i = 0;; i++){
 		sent = (char*)realloc(sent, (i + 1) * sizeof(char));
 		scanf("%c", &sent[i]);
@@ -40,7 +40,7 @@ void main(){
 			break;
 		}
 
-		// Разделители
+		// Разделители.
 		if (!CheckInput(sent[i]) && CheckInput(sent[i - 1]) && ((i - 1) >= 0)){
 			sent[i] = '\0';
 			wordCounter++;
@@ -48,12 +48,12 @@ void main(){
 	}
 
 
-	// Записывает слова из предложения в новый массив
+	// Записывает слова из предложения в новый массив.
 	words = (char**)malloc(wordCounter * sizeof(char*));
 	for (i = 0; i < wordCounter; i++){
-		// Считает количество букв в слове
+		// Считает количество букв в слове.
 		for (j += 1; sent[j - 1] != '\0'; j++);
-		// Выделяется память и копируется слово
+		// Выделяется память и копируется слово.
 		words[i] = (char*)malloc((j - oldLen) * sizeof(char));
 		IsAllocated(words[i]);
 
@@ -66,72 +66,84 @@ void main(){
 	// Память для массива из структур Word.
 	aWords = (Word_t*)malloc(wordCounter * sizeof(Word_t));
 	IsAllocated(aWords);
-	
+
+	listHead = (List_t **)calloc(1, sizeof(List_t *));
+	IsAllocated(listHead);
+	*(listHead + k) = NULL;
+
 	// Запись каждого слова. 
 	for (i = 0; i < wordCounter; i++){
 		(aWords + i)->word = *(words + i);
 		(aWords + i)->fLet = **(words + i);
 		// Последняя буква в слове.
 		(aWords + i)->lLet = words[i][(strlen(*(words + i)) - 1)];
+
+		queueHead = AddToEnd(queueHead, aWords + i);
 	}
 
-	for (i = 0; i < wordCounter; i++){
+	for (i = 0; queueHead != NULL; i++){
 
-		fAdded = 0;
-
-		// Инициализируем голову списка.
-		if (listHead == NULL){
-			listHead = AddToEnd(listHead, aWords);
-			currentList = (List_t *)malloc(sizeof(List_t));
-			IsAllocated(currentList);
-			currentList = listHead;
-			continue;
+		if (queueHead != NULL && i != 0){
+			k++;
+			listHead = (List_t**)realloc(listHead, (k + 1) * sizeof(List_t*));
+			IsAllocated(listHead);
+			*(listHead + k) = NULL;
 		}
 
-		while (currentList->next != NULL){
-			if ((aWords + i)->fLet == currentList->word_S->lLet &&
-				(aWords + i)->lLet == currentList->next->word_S->fLet){
+		curQueue = queueHead;
+		currentList = *(listHead + k);
 
-				AddBet(currentList, currentList->next, aWords + i);
-				AddFromQ(listHead, listHead->next, queueHead);
-				currentList = currentList->next;
-				fAdded = 1;
-				break;
+		// Инициализируем голову списка
+		if (*(listHead + k) == NULL){
+			*(listHead + k) = (List_t*)calloc(1, sizeof(List_t));
+			IsAllocated(*listHead + k);
+			(*(listHead + k))->word_S = curQueue->word_S;
+			currentList = *(listHead + k);
+
+			DeleteSpec(&queueHead, curQueue);
+			curQueue = queueHead;
+			listsA++;
+		}
+		/* 
+			Исправить бесконечный цикл если остаются разные элементы
+			Так же, возможно, появится проблема со вторым условием
+		*/
+		while (curQueue != NULL){
+			if (currentList->word_S->fLet == curQueue->word_S->lLet){
+				AddToBegin(&currentList, curQueue->word_S);
+				DeleteSpec(&queueHead, curQueue);
 			}
-
-			currentList = currentList->next;
-		}
-
-		if (fAdded) continue;
-
-		if ((aWords + i)->fLet == currentList->word_S->lLet){
-			AddToEnd(listHead, aWords + i);
-			AddFromQ(listHead, listHead->next, queueHead);
-		}else if ((aWords + i)->lLet == currentList->word_S->fLet){
-			AddToBegin(&listHead, aWords + i);
-			AddFromQ(listHead, listHead->next, queueHead);
-		}else{
-			if (queueHead == NULL){
-				queueHead = AddToEnd(queueHead, aWords + i);
-				continue;
+			else if (currentList->word_S->lLet == curQueue->word_S->fLet &&
+				currentList->next->word_S->fLet == curQueue->word_S->lLet){
+				if (currentList != NULL && currentList->next != NULL){
+					AddBet(currentList, currentList->next, curQueue->word_S);
+					DeleteSpec(&queueHead, curQueue);
+				}
+			}else if (currentList->word_S->lLet == curQueue->word_S->fLet){
+				AddToEnd(currentList, curQueue->word_S);
+				DeleteSpec(&queueHead, curQueue);
+			}else{
+				AddToEnd(queueHead, curQueue->word_S);
+				DeleteSpec(&queueHead, curQueue);
 			}
-
-			AddToEnd(queueHead, aWords + i);
+			
+			curQueue = curQueue->next;
 		}
+		// Continue...
 	}
+
+	
 #pragma endregion
 
-	// Вывод
-	printf("\nList: ");
-	while (listHead != NULL){
-		printf("%s ", listHead->word_S->word);
-		listHead = listHead->next;
-	}
-
-	printf("\nQueue: ");
-	while (queueHead != NULL){
-		printf("%s ", queueHead->word_S->word);
-		queueHead = queueHead->next;
+	// Вывод.
+	for (i = 0; i < listsA; i++){
+		printf("\nList #%d: ", i + 1);
+		printf("%s ", (*(listHead + i))->word_S->word);
+		currentList = (*(listHead + i));
+		while (currentList->next != NULL){
+			printf("%s ", currentList->next->word_S->word);
+			currentList = currentList->next;
+		}
 	}
 	printf("\n");
 
@@ -161,30 +173,29 @@ void IsAllocated(void *mem){
 	}
 }
 
-// Добавление из очереди.
-void AddFromQ(List_t *first, List_t *second, List_t *queueHead){
-	List_t *currentQ = queueHead;
-	List_t *currentP = first;
-	List_t *currentN = second;
-
-	while (currentN->next != NULL){
-		if (currentQ->word_S->fLet == currentP->word_S->lLet &&
-			currentQ->word_S->lLet == currentN->word_S->fLet){
-			
-			AddBet(currentP, currentN, currentQ->word_S);
-			currentQ = currentQ->next;
-		}else if (currentQ != NULL){
-			currentQ = queueHead;
-			currentP = currentN;
-			currentN = currentN->next;
+void DeleteSpec(List_t **head, List_t *del_el){
+	List_t *t = del_el;
+	List_t *current = *head;
+	if (t == *head){
+		*head = t->next;
+		del_el = NULL;
+		free(del_el);
+	}else{
+		if (current->next != NULL){
+			while (current->next != del_el){
+				current = current->next;
+			}
 		}
+		if (!(t->next)){
+			current->next->next = NULL;
+		}
+		else{
+			current->next = del_el->next;
+		}
+		t = NULL;
+		free(t);
 	}
 
-	if (currentQ->word_S->fLet == currentN->word_S->lLet){
-		AddToEnd(first, queueHead->word_S);
-	}else if (currentQ->word_S->lLet == first->word_S->fLet){
-		AddToBegin(&first, queueHead->word_S);
-	}
 }
 
 // Добавление между элементами.
@@ -200,7 +211,7 @@ void AddBet(List_t *prev, List_t *next, Word_t *word){
 // Добавление слова в конец списка.
 List_t * AddToEnd(List_t *head, Word_t *word){
 	List_t *current = head;
-
+	
 	if (current == NULL){
 		current = (List_t *)malloc(sizeof(List_t));
 		IsAllocated(current);
@@ -214,7 +225,7 @@ List_t * AddToEnd(List_t *head, Word_t *word){
 	while (current->next != NULL){
 		current = current->next;
 	}
-
+	
 	current->next = (List_t*)malloc(sizeof(List_t));
 	IsAllocated(current->next);
 
