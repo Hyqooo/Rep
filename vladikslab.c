a6515e19b5bf1e2a34b0d22e4760c1e22fa4a789
Hyqooo <33322066+Hyqooo@users.noreply.github.com>
2017-11-14 22:50:36 +0300

    Update vladikslab.c

--- vladikslab.c
+++ vladikslab.c
@@ -13,6 +13,7 @@ void main(){
 	
 	if (cur == NULL){
 		cur = (List *)malloc(sizeof(List));
+		if (cur == NULL) return;
 		scanf("%i", &value);
 		cur->val = value;
 		cur->next = NULL;
@@ -22,6 +23,7 @@ void main(){
 
 	while (1){
 		cur->next = (List *)malloc(sizeof(List));
+		if (cur->next == NULL) return;
 		scanf("%i%c", &value, &c);
 		cur->next->val = value;
 		cur->next->next = NULL;
